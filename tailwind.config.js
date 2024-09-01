a222bd292169b51b66629df4f052762865539488
Khushalmishra11 <khushalmishra261203@gmail.com>
2024-09-01 22:02:03 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+@type {import('tailwindcss').Config}
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./dist/*.html,js"],
   theme: {
     extend: {},
   },
